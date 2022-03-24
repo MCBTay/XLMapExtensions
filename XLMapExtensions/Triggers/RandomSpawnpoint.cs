5f25aebc4724ba9ab34f9b97e126cf6fd22b9eab
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-24 15:22:35 +0100

    adding RandomSpawnpoint

---
+++ XLMapExtensions/Triggers/RandomSpawnpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnpoint : MonoBehaviour
+{
+    // Vector 3 position
+    public Gameobject[] objects;
+
+    void Start()
+    {
+        objects = (GameObject.FindGameObjectsWithTag("SpawnPoint"));
+
+        int randomNumber = Random.Range(0, (objects.Length - 1));
+        Debug.Log(objects[randomNumber].transform.position);
+
+
+
+        
+    }
+}
