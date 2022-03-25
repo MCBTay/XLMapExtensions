56c252cbb0d6e398537c51d7d5a521cfe0550d8a
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-25 18:27:02 +0100

    code update

--- XLMapExtensions/Triggers/RandomSpawnpoint.cs
+++ XLMapExtensions/Triggers/RandomSpawnpoint.cs
@@ -1,21 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityModManagerNet;
 
-public class RandomSpawnpoint : MonoBehaviour
+namespace XLMapExtensions.Triggers
 {
-    // Vector 3 position
-    public Gameobject[] objects;
-
-    void Start()
+    [Serializable]
+    public class RandomSpawnpoint : MonoBehaviour
     {
-        objects = (GameObject.FindGameObjectsWithTag("SpawnPoint"));
+        private GameObject[] objects;
+        private Transform spawnLocation;
+        private int randomNumber;
 
-        int randomNumber = Random.Range(0, (objects.Length - 1));
-        Debug.Log(objects[randomNumber].transform.position);
+        void Start()
+        {
+            objects = (GameObject.FindGameObjectsWithTag("SpawnPoint"));
 
+            randomNumber = UnityEngine.Random.Range(0, (objects.Length - 1));
+            spawnLocation = objects[randomNumber].transform;
 
+/*            StartCoroutine(DelaySpawn(5f));
 
-        
+            for (int i = 0; i < objects.Length; i++)
+            {
+                UnityModManager.Logger.Log(objects[i].transform.position.ToString());
+            }*/
+        }
+
+        void Update()
+        {
+            if(PlayerController.Instance.respawn.recentlyRespawned == true)
+            {
+                /*PlayerController.Instance.respawn.recentlyRespawned = false;*/
+                UnityModManager.Logger.Log("recentlyRespawned == true");
+                
+                PlayerController.Instance.respawn.SetSpawnPos(spawnLocation.position, spawnLocation.rotation);
+                PlayerController.Instance.respawn.DoRespawn();
+
+                UnityModManager.Logger.Log(randomNumber.ToString());
+                UnityModManager.Logger.Log(spawnLocation.position.ToString());
+                gameObject.GetComponent<RandomSpawnpoint>().enabled = false;
+            }
+        }
+
+/*        IEnumerator DelaySpawn(float time)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            PlayerController.Instance.respawn.SetSpawnPos(spawnLocation.position, spawnLocation.rotation);
+            PlayerController.Instance.respawn.DoRespawn();
+            gameObject.GetComponent<RandomSpawnpoint>().enabled = false;
+        }*/
     }
 }
