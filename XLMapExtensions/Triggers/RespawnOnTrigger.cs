907710f817c902f5f47218e8772b51ec06f93b40
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-26 15:27:07 +0100

    added RespawnOnTrigger

---
+++ XLMapExtensions/Triggers/RespawnOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace XLMapExtensions.Triggers
+{
+    [Serializable]
+    public class RespawnOnTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider collider)
+        {
+            PlayerController.Instance.respawn.DoRespawn();
+        }
+    }
+}
