c382f42c3d4d3fa63ab94fd4553643e90a8cbd44
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-28 15:11:26 +0200

    adding random teleport on trigger

---
+++ XLMapExtensions/Triggers/RandomTeleportOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XLMapExtensions.Triggers
+{
+    [Serializable]
+    public class RandomTeleportOnTrigger : BoardTriggerBase
+    {
+        public List<Transform> tpLocations = new List<Transform>();
+        private int randomNumber;
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider != _boardCollider) return;
+            if (!CanBeFiredAgain()) return;
+            if (tpLocations.Count == 0) return;
+
+            randomNumber = UnityEngine.Random.Range(0, tpLocations.Count);
+            PlayerController.Instance.respawn.SetSpawnPos(tpLocations[randomNumber].position, tpLocations[randomNumber].rotation);
+            PlayerController.Instance.respawn.DoRespawn();
+        }
+    }
+}
