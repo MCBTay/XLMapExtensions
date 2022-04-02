e356c9481fac95b5a3449ce70d8c46f9d7131cb1
mcbtay <mcbtay@gmail.com>
2022-04-02 09:52:12 -0400

    adding Disable In Replay script

---
+++ XLMapExtensions/DisableInReplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GameManagement;
+using UnityEngine;
+
+namespace XLMapExtensions
+{
+[Serializable]
+public class DisableInReplay : MonoBehaviour
+{
+    private void Update()
+    {
+        if (gameObject == null) return;
+
+        switch (GameStateMachine.Instance.CurrentState)
+        {
+            case PlayState playState:
+                if (!gameObject.activeSelf) gameObject.SetActive(true);
+                break;
+            case ReplayState replayState:
+                if (gameObject.activeSelf) gameObject.SetActive(false);
+                break;
+        }
+    }
+}
+}
