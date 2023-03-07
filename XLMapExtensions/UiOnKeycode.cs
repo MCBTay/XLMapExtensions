1e5b49a082584edb94ee367eb61ad7ba004d7e5d
Brookzee99 <Shaunfarley2@googlemail.com>
2023-03-07 23:34:24 +0000

    Added CountdownTimer & UiOnKeycode
    Countdown Timer allows users to set a timer and update UI text.
    
    UiOnKeyboard allows users to have a UI menu appear in game by pressing a keyboard command

---
+++ XLMapExtensions/UiOnKeycode.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+namespace UiOnKeycode
+
+{
+    public class UiOnKeycode : MonoBehaviour
+    {
+        [Header("Ui Element")]
+        [Tooltip("Place your UI panel here")]
+        public GameObject UiMenu;
+        [Header("Control To Open Ui Menu")]
+        [Tooltip("Select the Keyboard command to open your UI")]
+        [SerializeField] private KeyCode MenuKey = KeyCode.H;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(MenuKey))
+
+            {
+                UiMenu.SetActive(!UiMenu.activeInHierarchy);
+                Cursor.visible = UiMenu.activeInHierarchy;
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = UiMenu.activeInHierarchy ? 0f : 1f;
+            }
+
+            if (UiMenu.activeInHierarchy)
+            {
+                AudioListener.pause = true;
+            }
+            else
+            {
+                AudioListener.pause = false;
+            }
+        }
+
+    }
+}
